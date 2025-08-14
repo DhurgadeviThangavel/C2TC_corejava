f529107ff487d99f957d4083fb6d3d3f1b71fcd7
OMEN <OMEN@LAPTOP-G6BSE1K2>
2025-08-14 23:16:19 +0530

    commit

---
+++ C2TC_corejavaprogram/src/Day2/Execute.java
@@ -0,0 +1,17 @@
+package Day2;
+
+public class Execute {
+
+	public static void main(String[] args) {
+		Person p =new Person();
+		p.setId(101);
+		p.setName("Ram");
+		p.setAge(20);
+		p.setCity("Puducherry");
+		
+		System.out.println(p.toString());
+		System.out.println(p);
+
+	}
+
+}
