5d1d577f69aabe94afe51adef5dd294a1b9fda9f
OMEN <OMEN@LAPTOP-G6BSE1K2>
2025-08-18 16:25:09 +0530

    Commit

---
+++ C2TC_corejavaprogram/src/Daythree/BufferReader/FileReaderDemo.java
@@ -0,0 +1,18 @@
+package Daythree.BufferReader;
+
+import java.io.*;  
+
+public class FileReaderDemo {
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        FileReader fr = new FileReader("data.txt"); 
+        BufferedReader br = new BufferedReader(fr);
+        
+        String content ;
+        while((content = br.readLine())!=null) {
+            System.out.println(content);
+        }
+        br.close(); 
+        fr.close();
+    }
+}
